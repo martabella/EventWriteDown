7eea1a3e93caae86562baa9ddea19a2689c86b84
martabella <mbella@lasalle.cat>
2015-08-15 12:48:07 +0200

    03 Enable user interaction (add, minus, and reset buttons)

--- app/src/main/java/com/example/mbella/eventwritedown/MainActivity.java
+++ app/src/main/java/com/example/mbella/eventwritedown/MainActivity.java
@@ -1,19 +1,84 @@
 package com.example.mbella.eventwritedown;
 
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private long numMen=0; //Men count
+    private long numWomen=0; //Women count
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * This method is called when the delete Name button is clicked.
+     * Allows delete the edit text name
+     **/
+    public void deleteName (View view){
+        EditText name_field = (EditText)this.findViewById(R.id.name_field);
+        name_field.setText("");
+    }
+    /**
+     * This method is called when the delete quantity button is clicked.
+     * Allows set the men/women quantity to 0. Sets the quantity_text_view to initial value.
+     **/
+    public void deleteQuantity (View view){
+        numMen=0;
+        numWomen=0;
+        this.updateQuantityTextView();
+    }
+    /**
+     * This method is called when the decrement button is clicked.
+     * Allows decrement the men/women values. Depends on radiobutton selection.
+     * Updates the quantity_text_view.
+     **/
+    public void decrementValues(View view){
+        RadioButton male_radiobutton = (RadioButton) this.findViewById(R.id.male_radiobutton);
+        if (male_radiobutton.isChecked()){
+            numMen--;
+        }else{
+            //female_button is checked
+            numWomen--;
+        }
+        this.updateQuantityTextView();
+    }
+    /**
+     * This method is called when the decrement button is clicked.
+     * Allows increment the men/women values. Depends on radiobutton selection.
+     * Updates the quantity_text_view.
+     **/
+    public void addValues(View view){
+        RadioButton male_radiobutton = (RadioButton) this.findViewById(R.id.male_radiobutton);
+        if (male_radiobutton.isChecked()){
+            numMen++;
+        }else{
+            //female_button is checked
+            numWomen++;
+        }
+        this.updateQuantityTextView();
+    }
 
+    /**
+     * Updates the quantity_text_view reading  numMen and numWomen values
+     */
+    private void updateQuantityTextView(){
+        TextView quantity_text_view = (TextView)this.findViewById(R.id.quantity_text_view);
+        quantity_text_view.setText(numMen+" "+this.getResources().getString(R.string.men)
+                +" / "+numWomen+" "+this.getResources().getString(R.string.women));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +101,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
